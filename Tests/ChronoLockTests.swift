71ac248a674724d8c691b33dd61469bb43878d47
Cristian Rojas <cristian.rojas@live.fr>
2025-05-30 15:43:09 +0200

    Add test target

---
+++ Tests/ChronoLockTests.swift
@@ -0,0 +1,11 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 30/5/25.
+
+import XCTest
+
+class ChronoLockTests: XCTestCase {
+    func test() {
+        XCTExpectFailure {
+            XCTFail()
+        }
+    }
+}
