7a67192be5355a407fe1ae12dd2fb3d0ba4fc5da
Cristian Rojas <cristian.rojas@live.fr>
2025-05-30 16:32:13 +0200

    Encrypt and decrypt with same passphrase returns original message

--- Tests/ChronoLockTests.swift
+++ Tests/ChronoLockTests.swift
@@ -1,11 +1,36 @@
 // © 2025  Cristian Felipe Patiño Rojas. Created on 30/5/25.
 
 import XCTest
+import CryptoKit
 
 class ChronoLockTests: XCTestCase {
-    func test() {
-        XCTExpectFailure {
-            XCTFail()
+    
+    struct Encryptor {
+        let passphrase: String
+        
+        private var key: SymmetricKey {
+            let passphrase = "ChronoLockKey123"
+            let keyData = SHA256.hash(data: Data(passphrase.utf8))
+            return SymmetricKey(data: keyData)
         }
+
+        func encrypt(_ message: String) throws -> Data {
+            let messageData = message.data(using: .utf8)!
+            let sealedBox = try AES.GCM.seal(messageData, using: key)
+            return sealedBox.combined!
+        }
+
+        func decrypt(_ data: Data) throws -> String {
+            let sealedBox = try AES.GCM.SealedBox(combined: data)
+            let decryptedData = try AES.GCM.open(sealedBox, using: key)
+            return String(data: decryptedData, encoding: .utf8)!
+        }
+    }
+    
+    func test_encryptAndDecrypt_withSamePassphrase_returnsOriginalMessage() throws {
+        let sut = Encryptor(passphrase: "test phrase")
+        let encrypted = try sut.encrypt("hello world")
+        let decrypted = try sut.decrypt(encrypted)
+        XCTAssertEqual(decrypted, "hello world")
     }
 }
